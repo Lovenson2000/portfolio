4c485b036e913c22c5b84a40809f6158468f4545
Lovenson2000 <beaucicotlovenson@gmail.com>
2023-07-11 12:12:13 +0800

    Updating animations

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 
+//Making the Nav Bar Responsive
+
+const navContainer = document.getElementById("nav-container");
+const hamburger = document.getElementById("hamburger");
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navContainer.classList.toggle("active");
+
+})
+
+document.querySelectorAll(".nav-link-wrapper").forEach((link) => {
+    link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navContainer.classList.remove("active");
+    })
+
+})
+
 //Animating the nav bar items with scrollReveal()
 const sr = ScrollReveal();
 
 sr.reveal(".nav-link-wrapper", {
   delay: 200,
-  duration: 1500,
+  duration: 1100,
   distance: "50px",
   origin: "top",
   interval: 400,
@@ -13,13 +31,43 @@ sr.reveal(".nav-link-wrapper", {
 
 
 //Animating the hero-section
-sr.reveal(".hero-container", {
-    delay: 2000,
+sr.reveal(".name-heading", {
+    delay: 200,
+    duration: 1000,
+    distance: "50px",
+    interval: 400,
+    origin: "left",
+    easing: 'ease-in',
+    mobile: true
+});
+
+sr.reveal(".main-heading", {
+    delay: 300,
     duration: 1500,
     distance: "50px",
     interval: 400,
+    origin: "right",
     easing: 'ease-in',
-    mobile: false
+    mobile: true
+});
+sr.reveal(".my-details", {
+    delay: 400,
+    duration: 1100,
+    distance: "50px",
+    interval: 400,
+    origin: "left",
+    easing: 'ease-in',
+    mobile: true
+});
+
+sr.reveal(".button", {
+    delay: 500,
+    duration: 1100,
+    distance: "50px",
+    interval: 400,
+    origin: "right",
+    easing: 'ease-in',
+    mobile: true
 });
 
 sr.reveal(".hero-container", {
@@ -52,28 +100,8 @@ sr.reveal(".about-photo", {
     mobile: true,
     easing: 'ease-out',
     viewFactor: 0.4,
-    
  
 });
 
 
-
-
-//Making the Nav Bar Responsive
-
-const navContainer = document.getElementById("nav-container");
-const hamburger = document.getElementById("hamburger");
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navContainer.classList.toggle("active");
-
-})
-
-document.querySelectorAll(".nav-link-wrapper").forEach((link) => {
-    link.addEventListener("click", () => {
-        hamburger.classList.remove("active");
-        navContainer.classList.remove("active");
-    })
-
-})
 
